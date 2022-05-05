a0e328b55e49bf4d4f308fb5f1a83402c6fa6e13
MummineniTejesh <tejeshmummineni@gmail.com>
2022-05-05 19:33:48 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// const convs=[
+//     {name: "tom",time:"8:30",message:"good morning"},
+//     {name: "tom",time:"8:30",message:"good morning"},
+//     {name: "tom",time:"8:30",message:"good morning"},
+//     {name: "tom",time:"8:30",message:"good morning"},
+//     {name: "tom",time:"8:30",message:"good morning"}
+// ]
+
+
+let conv=document.getElementById("conv");
+let cloneElm = menu.cloneNode(true);
+document.body.appendChild(cloneElm);
